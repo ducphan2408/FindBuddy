17c33142419bec77adbd24da6b7483639f50afb1
gerphan <germanphan2002@gmail.com>
2023-04-17 10:02:10 +0700

    animation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Image} from 'react-native';
+import LottieView from "lottie-react-native";
+// import {Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Onboarding from 'react-native-onboarding-swiper';
@@ -9,6 +10,7 @@ function HomeScreen() {
     <Text>Home</Text>
   );
 }
+
 function OnBoardingScreen() {
   return (
     <Onboarding
@@ -17,24 +19,27 @@ function OnBoardingScreen() {
      pages={[
     {
       backgroundColor: '#f2f4f6',
-      image: <Image  source={require('./assets/first.png')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,
+      image: <LottieView autoPlay loop source={require('./assets/48646-girl-running.json')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,
       title: 'FIND YOUR BUDDY',
       subtitle: 'Feature that connects users with like-minded individuals based on shared interests and goals, fostering new friendships and fitness partnerships.',
     },
+
     {
       backgroundColor: '#f2f4f6',
-      image: <Image source={require('./assets/second.png')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,
+      image: <LottieView autoPlay loop source={require('./assets/42941-man-running.json')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,      
       title: 'HEALTHIER BODY AND MIND',
       subtitle: 'A lounge for runners around the world to connect and share their love of running.',
     },
+    
     {
       backgroundColor: '#f2f4f6',
-      image: <Image source={require('./assets/third.png')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,
+      image: <LottieView autoPlay loop source={require('./assets/36222-couple.json')} style={{height:300, width:300, backgroundColor:'#f2f4f6'}} />,
       title: 'THE JOY RUNNING TOGETHER',
       subtitle: 'Find your perfect running buddy with our app, stay motivated, reach your fitness goals, and make new friends!',
     },
   ]}
 />
+
   )
 }
 const Stack = createNativeStackNavigator();
@@ -49,4 +54,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
